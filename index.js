75b67bac46091c002ba29d374e250e12687e361d
Kyle Greenidge <kylegreen149@gmail.com>
2024-07-16 15:19:03 -0400

    Done

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (num1, num2) {
+    let quotient = num1 / num2
+    return quotient
+}
+
+const square = numToMultiply => numToMultiply ** numToMultiply
+
+const add = (num3, num4) => {
+    let sum = num3 + num4
+    return sum
+}
